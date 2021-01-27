7c72b562ac1ecdbc52d0e89f11a54a4f1f069c34
Duc Cao <caovanducs@gmail.com>
2021-01-27 20:32:02 +0700

    Linked List Insert Tail

--- LinkedListProblems/1.InsertTail.js
+++ LinkedListProblems/1.InsertTail.js
@@ -4,6 +4,46 @@ class NodeList {
     this.next = null;
   }
 }
-class LinkedList(){
-    
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
 }
+
+function insertAtTail(head, data) {
+  const newNode = new NodeList(data);
+
+  if (head === null) {
+    head = newNode;
+    return head;
+  }
+
+  let current = new NodeList();
+  current = head;
+
+  while (current.next !== null) {
+    current = current.next;
+  }
+
+  current.next = newNode;
+
+  console.log("Current", current);
+
+  console.log(head);
+
+  return head;
+}
+
+function main() {
+  let llist = new LinkedList();
+  for (let i = 0; i < 5; ++i) {
+    const llistitem = i;
+    const llistHead = insertAtTail(llist.head, llistitem);
+    llist.head = llistHead;
+  }
+
+  //console.log("result ", llist.head);
+}
+
+main();
