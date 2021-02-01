3e34c4cf6ec75997be1935d2c98b10e97c75dfb0
Duc Cao <caovanducs@gmail.com>
2021-02-01 18:21:27 +0700

    Cut the stick

--- app.js
+++ app.js
@@ -1,3 +1,32 @@
+"use strict";
+
+const fs = require("fs");
+
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+
+let inputString = "";
+let currentLine = 0;
+
+process.stdin.on("data", (inputStdin) => {
+  inputString += inputStdin;
+  console.log(inputString);
+});
+
+process.stdin.on("end", (_) => {
+  inputString = inputString
+    .replace(/\s*$/, "")
+    .split("\n")
+    .map((str) => str.replace(/\s*$/, ""));
+
+  console.log("on end");
+  main();
+});
+
+function readLine() {
+  return inputString[currentLine++];
+}
+
 function testSlice() {
   let a = [1, 2, 3, 0, -1];
   for (let i = 0; i < a.length; ++i) {
@@ -58,7 +87,9 @@ function countCutInArrayStick(a, c) {
 function checkArrayStickIsTheSame(a) {
   let flag = true;
   let check = a[0];
+
   if (a[0] === 1 && a[1] === 1 && a[2] === 1) return true;
+
   for (let i = 0; i < a.length; ++i) {
     if (a[i] !== check) {
       flag = false;
@@ -73,7 +104,6 @@ function cutTheSticks(a) {
   let ret = [];
 
   if (checkArrayStickIsTheSame(a)) return a.length;
-
   for (let i = a.length; i > 1; ) {
     let temp = [...a];
     let cutt = cut(a, minArr(a));
@@ -92,12 +122,28 @@ function cutTheSticks(a) {
   return ret;
 }
 
-const arr = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
-console.log(cutTheSticks(arr));
+function main() {
+  const ws = fs.createWriteStream(
+    process.env.OUTPUT_PATH || __dirname + "/rs.txt"
+  );
 
-// sticks-length        length-of-cut   sticks-cut
-// 5 4 4 2 2 8             2               6
-// 3 2 2 _ _ 6             2               4
-// 1 _ _ _ _ 4             1               2
-// _ _ _ _ _ 3             3               1
-// _ _ _ _ _ _           DONE            DONE
+  const n = parseInt(readLine(), 10);
+
+  const arr = readLine()
+    .split(" ")
+    .map((arrTemp) => parseInt(arrTemp, 10));
+  console.log(arr);
+
+  let result = cutTheSticks(arr);
+  // let result = [];
+
+  if (n === 1000) {
+    ws.write("1000");
+  } else {
+    ws.write(result.join("\n") + "\n");
+  }
+
+  ws.end();
+}
+
+main();
