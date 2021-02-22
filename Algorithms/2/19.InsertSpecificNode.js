098619d96edcc2a96558151217d4375014a706d4
Duc Cao <caovanducs@gmail.com>
2021-02-22 15:05:42 +0700

    Insert Specification Pos LinkedList

---
+++ Algorithms/2/19.InsertSpecificNode.js
@@ -0,0 +1,111 @@
+"use strict";
+
+const fs = require("fs");
+
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+
+let inputString = "";
+let currentLine = 0;
+
+process.stdin.on("data", (stdin) => {
+  inputString += stdin;
+});
+
+process.on("SIGINT", function () {
+  inputString = inputString
+    .replace(/\s*$/, "")
+    .split("\n")
+    .map((s) => s.replace(/\s*$/, ""));
+
+  main();
+  console.log("Ended Process!");
+  process.exit(1);
+});
+
+function readLine() {
+  return inputString[currentLine++];
+}
+
+class SinglyLinkedListNode {
+  constructor(nodeData) {
+    this.data = nodeData;
+    this.next = null;
+  }
+}
+
+class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+
+  insertNode(nodeData) {
+    const node = new SinglyLinkedListNode(nodeData);
+
+    if (this.head === null) {
+      this.head = node;
+    } else {
+      this.tail.next = node;
+    }
+    this.tail = node;
+  }
+}
+
+function printSinglyLinkedList(node, sep, ws) {
+  while (node !== null) {
+    ws.write(String(node.data));
+
+    node = node.next;
+
+    if (node !== null) {
+      ws.write(sep);
+    }
+  }
+}
+
+function insertNodeAtPosition(headPure, data, position) {
+  const newNode = new SinglyLinkedListNode(data);
+  let head = headPure;
+  let c = 0;
+
+  while (head !== null) {
+    console.log(head);
+
+    if (c === position) {
+      const savePos = head.next;
+      head.next = newNode;
+      newNode.next = savePos;
+      break;
+    } else {
+      head = head.next;
+      c++;
+    }
+  }
+
+  return headPure;
+}
+
+function main() {
+  const ws = fs.createWriteStream(__dirname + "/output.txt");
+
+  const llistCount = parseInt(readLine(), 10);
+
+  let llist = new SinglyLinkedList();
+
+  for (let i = 0; i < llistCount; i++) {
+    const llistItem = parseInt(readLine(), 10);
+    llist.insertNode(llistItem);
+  }
+
+  const data = parseInt(readLine(), 10);
+
+  const position = parseInt(readLine(), 10);
+
+  let llist_head = insertNodeAtPosition(llist.head, data, position);
+
+  printSinglyLinkedList(llist_head, " ", ws);
+  ws.write("\n");
+
+  ws.end();
+}
---
+++ Favorites/1_RandsomeNote.js
@@ -0,0 +1,64 @@
+"use strict";
+
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+
+let inputString = "";
+let currentLine = 0;
+
+process.stdin.on("data", (inputStdin) => {
+  inputString += inputStdin;
+});
+
+process.on("SIGINT", function () {
+  inputString = inputString
+    .replace(/\s*$/, "")
+    .split("\n")
+    .map((str) => str.replace(/\s*$/, ""));
+
+  main();
+  process.stdout.write("Ended STDIN\n");
+  process.exit();
+});
+
+function readline() {
+  return inputString[currentLine++];
+}
+
+// Complete the checkMagazine function below.
+function f(x, y) {
+  var map = {};
+  var rep = true;
+
+  for (let i of x) {
+    map[i] = (map[i] || 0) + 1;
+  }
+
+  for (let j of y) {
+    map[j] = (map[j] || 0) - 1;
+  }
+
+  for (let k in map) {
+    console.log("k = ", k);
+    if (map[k] < 0) {
+      rep = false;
+      break;
+    }
+  }
+
+  console.log(rep ? "Yes" : "No");
+}
+
+function main() {
+  const mn = readline().split(" ");
+
+  const m = parseInt(mn[0], 10);
+
+  const n = parseInt(mn[1], 10);
+
+  const x = readline().split(" ");
+
+  const y = readline().split(" ");
+
+  f(x, y);
+}
